f53db8294a3c12b2f4c7110fbd3b09dce1dfe6f2
aayush2456 <jaayush21@tbc.edu.np>
2021-12-10 23:40:44 +0545

    second task completed

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,13 +11,18 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG,"Button Clicked");
+        Log.d(LOG_TAG,"Button Clicked!");
+        Intent intent = new Intent(this, SecondActivitty.class);
+        startActivity(intent);
     }
 }
